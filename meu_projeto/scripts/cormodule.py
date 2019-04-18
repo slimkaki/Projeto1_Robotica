296079a9672710a263ebb7ba9127230a6e4fc7f3
lucas leal vale <lucaslealvale01@gmail.com>
2019-04-18 15:22:56 -0300

    rubrica B

--- meu_projeto/scripts/cormodule.py
+++ meu_projeto/scripts/cormodule.py
@@ -16,70 +16,6 @@ import smach
 import smach_ros
 import auxiliar as aux
 
-
-
-def identifica_cor_vermelho(frame):
-	'''
-	Segmenta o maior objeto cuja cor é parecida com cor_h (HUE da cor, no espaço HSV).
-	'''
-
-	# No OpenCV, o canal H vai de 0 até 179, logo cores similares ao 
-	# vermelho puro (H=0) estão entre H=-8 e H=8. 
-	# Precisamos dividir o inRange em duas partes para fazer a detecção 
-	# do vermelho:
-	frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-	#cor_menor = np.array([0, 50, 50])
-	#cor_maior = np.array([8, 255, 255])
-	cor_menor, cor_maior = aux.ranges('#FF0000')
-	segmentado_cor = cv2.inRange(frame_hsv, cor_menor, cor_maior)
-
-	# cor_menor = np.array([172, 50, 50])
-	# cor_maior = np.array([180, 255, 255])
-	# segmentado_cor += cv2.inRange(frame_hsv, cor_menor, cor_maior)
-
-
-	# A operação MORPH_CLOSE fecha todos os buracos na máscara menores 
-	# que um quadrado 7x7. É muito útil para juntar vários 
-	# pequenos contornos muito próximos em um só.
-	segmentado_cor = cv2.morphologyEx(segmentado_cor,cv2.MORPH_CLOSE,np.ones((7, 7)))
-
-	# Encontramos os contornos na máscara e selecionamos o de maior área
-	#contornos, arvore = cv2.findContours(segmentado_cor.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)	
-	img_out, contornos, arvore = cv2.findContours(segmentado_cor.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
-
-	maior_contorno = None
-	maior_contorno_area = 0
-
-	for cnt in contornos:
-	    area = cv2.contourArea(cnt)
-	    if area > maior_contorno_area:
-	        maior_contorno = cnt
-	        maior_contorno_area = area
-
-	# Encontramos o centro do contorno fazendo a média de todos seus pontos.
-	if not maior_contorno is None :
-	    cv2.drawContours(frame, [maior_contorno], -1, [0, 0, 255], 5)
-	    maior_contorno = np.reshape(maior_contorno, (maior_contorno.shape[0], 2))
-	    media = maior_contorno.mean(axis=0)
-	    media = media.astype(np.int32)
-	    cv2.circle(frame, tuple(media), 5, [0, 255, 0])
-	else:
-	    media = (0, 0)
-
-	# Representa a area e o centro do maior contorno no frame
-	font = cv2.FONT_HERSHEY_COMPLEX_SMALL
-	cv2.putText(frame,"{:d} {:d}".format(*media),(20,100), 1, 4,(255,255,255),2,cv2.LINE_AA)
-	cv2.putText(frame,"{:0.1f}".format(maior_contorno_area),(20,50), 1, 4,(255,255,255),2,cv2.LINE_AA)
-
-	cv2.imshow('Camera', frame)
-	cv2.imshow('seg', segmentado_cor)
-	cv2.waitKey(1)
-
-	centro = (frame.shape[0]//2, frame.shape[1]//2)
-
-	return media, centro, maior_contorno_area
-
 def identifica_cor_azul(frame):
 	'''
 	Segmenta o maior objeto cuja cor é parecida com cor_h (HUE da cor, no espaço HSV).
@@ -90,14 +26,14 @@ def identifica_cor_azul(frame):
 	# Precisamos dividir o inRange em duas partes para fazer a detecção 
 	# do vermelho:
 	frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-	cor_menor = np.array([98, 109, 20])
-	cor_maior = np.array([112, 255, 255])
-	segmentado_cor = cv2.inRange(frame_hsv, cor_menor, cor_maior)
+	cor_menor = np.array([80, 110, 100])
+	cor_maior = np.array([110, 255, 255])
+	#minCor, maxCor = aux.ranges('#4472c4')
+	#segmentado_cor = cv2.inRange(frame_hsv, minCor, maxCor)
 
 	# cor_menor = np.array([172, 50, 50])
 	# cor_maior = np.array([180, 255, 255])
-	# segmentado_cor += cv2.inRange(frame_hsv, cor_menor, cor_maior)
+	segmentado_cor = cv2.inRange(frame_hsv, cor_menor, cor_maior)
 
 
 	# A operação MORPH_CLOSE fecha todos os buracos na máscara menores 
@@ -120,7 +56,7 @@ def identifica_cor_azul(frame):
 
 	# Encontramos o centro do contorno fazendo a média de todos seus pontos.
 	if not maior_contorno is None :
-	    cv2.drawContours(frame, [maior_contorno], -1, [220, 100, 50 ], 5)
+	    cv2.drawContours(frame, [maior_contorno], -1, [210, 100, 100 ], 5)
 	    maior_contorno = np.reshape(maior_contorno, (maior_contorno.shape[0], 2))
 	    media = maior_contorno.mean(axis=0)
 	    media = media.astype(np.int32)
@@ -133,7 +69,7 @@ def identifica_cor_azul(frame):
 	cv2.putText(frame,"{:d} {:d}".format(*media),(20,100), 1, 4,(255,255,255),2,cv2.LINE_AA)
 	cv2.putText(frame,"{:0.1f}".format(maior_contorno_area),(20,50), 1, 4,(255,255,255),2,cv2.LINE_AA)
 
-	cv2.imshow('Camera', frame)
+	#cv2.imshow('Camera', frame)
 	cv2.imshow('seg', segmentado_cor)
 	cv2.waitKey(1)
 
--- meu_projeto/scripts/visao.py
+++ meu_projeto/scripts/visao.py
@@ -21,31 +21,19 @@ import visao_module
 
 
 batida=0
-#frontal=0
+Scaner=[0]*360
 v=0.1
 w =(pi/2)
-
-def bateu(dado):
-	global batida
-	batida = dado.data
-
-print("ta rodando o certo")
+w2 =0.3
 bridge = CvBridge()
-
 cv_image = None
-#mediaV = []
-#centroV = []
-
 mediaA = []
 centroA = []
-
 atraso = 0.5E9 # 1 segundo e meio. Em nanossegundos
-
 areaV = 0.0 # Variavel com a area do maior contorno
 areaA = 0.0
 metade=320
-sigma=25
-atraso = 1.5E9 # 1 segundo e meio. Em nanossegundos
+sigma=5
 media = []
 centro = []
 Pi=None
@@ -53,21 +41,25 @@ Pf=None
 posicao=None
 viu_dog = False
 media_dog=None
-# Só usar se os relógios ROS da Raspberry e do Linux desktop estiverem sincronizados. 
-# Descarta imagens que chegam atrasadas demais
-check_delay = False 
+check_delay = False
 
-# A função a seguir é chamada sempre que chega um novo frame
+def bateu(dado):
+	global batida
+	batida = dado.data
+
+def scaneou(dado):
+	global Scaner
+	Scaner = np.array(dado.ranges)
+
+	for i in range(len(Scaner)):
+		if Scaner[i] ==0.0:
+			Scaner[i]=4.0
+
 def roda_todo_frame(imagem):
-	#print("frame")
 	global cv_image
 	global mediaA
-	#global mediaV
 	global centroA
-	#global centroV
-	global areaV
 	global areaA
-
 	global media
 	global centro
 	global viu_dog
@@ -78,35 +70,27 @@ def roda_todo_frame(imagem):
 
 	now = rospy.get_rostime()
 	imgtime = imagem.header.stamp
-	lag = now-imgtime # calcula o lag
+	lag = now-imgtime
 	delay = lag.nsecs
-	#print("delay ", "{:.3f}".format(delay/1.0E9))
+
 	if delay > atraso and check_delay==True:
 		print("Descartando por causa do delay do frame:", delay)
 		return 
 	try:
 		antes = time.clock()
 		cv_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8")
-		#mediaV, centroV, areaV =  cormodule.identifica_cor_vermelho(cv_image)
 		mediaA, centroA, areaA =  cormodule.identifica_cor_azul(cv_image)
 		centro, imagem, resultados =  visao_module.processa(cv_image)
-        #vc_temp = find_circles(cv_image)
-        # if vc_temp:
-        #     viu_circulo = True
-
 
 		for r in resultados:
-		# print(r) - print feito para documentar e entender
-		# o resultado
-			if r[0] == "dog":
+			if r[0] == "cat":
 				viu_dog = True
 				Pi=r[2]
 				Pf=r[3]
 				posicao=(Pi[0],Pi[1],Pf[0],Pf[1])
-				media_dog=(posicao[2]-posicao[0])/2
-				#print("resultados")
+				media_dog=(posicao[0]+((posicao[2]-posicao[0]))/2)
+
 		depois = time.clock()
-		cv2.imshow("Camera", cv_image)
 
 	except CvBridgeError as e:
 		print('ex', e)
@@ -116,7 +100,8 @@ if __name__=="__main__":
 
 	topico_imagem = "/kamera"
 	recebe_Bumper = rospy.Subscriber("/bumper", UInt8, bateu)
-	
+	recebe_scan = rospy.Subscriber("/scan", LaserScan, scaneou)
+
 	# Para renomear a *webcam*
 	# 
 	# 	rosrun topic_tools relay  /cv_camera/image_raw/compressed /kamera
@@ -131,18 +116,20 @@ if __name__=="__main__":
 	# 
 
 	recebedor = rospy.Subscriber(topico_imagem, CompressedImage, roda_todo_frame, queue_size=4, buff_size = 2**24)
-	#print("Usando ", topico_imagem)
-
 	velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
 
 	try:
 
 		while not rospy.is_shutdown():
+
 			vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
 			
+			#BUMPERS - Survival
 			if batida != 0:
+
 				if batida == 2:
-					print("bati vou recalcular rota")
+
+					print("bati 2 vou recalcular rota")
 					vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
 					velocidade_saida.publish(vel)
 					rospy.sleep(1.0)
@@ -164,13 +151,14 @@ if __name__=="__main__":
 					rospy.sleep(1.0)
 
 					batida=0
+					continue
 
 				elif batida == 1:
-					print("bati vou recalcular rota")
+
+					print("bati 1 vou recalcular rota")
 					vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
 					velocidade_saida.publish(vel)
 					rospy.sleep(1.0)
-
 
 					vel = Twist(Vector3(-v,0,0), Vector3(0,0,0))
 					velocidade_saida.publish(vel)
@@ -189,9 +177,11 @@ if __name__=="__main__":
 					rospy.sleep(1.0)
 
 					batida=0
+					continue
 
 				elif batida == 3:
-					print("bati vou recalcular rota")
+
+					print("bati 3 vou recalcular rota")
 					vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
 					velocidade_saida.publish(vel)
 					rospy.sleep(1.0)
@@ -217,9 +207,11 @@ if __name__=="__main__":
 					rospy.sleep(4.0)
 
 					batida=0
+					continue
 
 				elif batida == 4:
-					print("bati vou recalcular rota")
+
+					print("bati 4 vou recalcular rota")
 					vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
 					velocidade_saida.publish(vel)
 					rospy.sleep(1.0)
@@ -245,102 +237,112 @@ if __name__=="__main__":
 					rospy.sleep(4.0)
 
 					batida=0
+					continue
 
-			if viu_dog:
-                #posicao xi, yi , xf, yf so me importa x
-				if posicao != None:
-					
-					#media_dog=(posicao[2]-posicao[0])/2
-					print(" a media da posicao do dog eh{0}".format(media_dog))
+			# DETECTOR DE PROXIMIDADE LASERSCAN - SURVIVAL
+			
+			#print("Scanner: "+str(Scaner))
+			Quad1=Scaner[0:90]
+			Quad2=Scaner[90:180]
+			Quad3=Scaner[180:270]
+			Quad4=Scaner[270:]
+			#split=np.split(Scaner,90)
 
-					if media_dog<metade+sigma:
-						vel = Twist(Vector3(0,0,0), Vector3(0,0,-0.3))
-						velocidade_saida.publish(vel)
-						rospy.sleep(0.8)
-						print("esquerda para ir ao dog")
+			#print(split)
 
-					elif media_dog>metade+sigma:
-						vel = Twist(Vector3(0,0,0), Vector3(0,0,0.3))
-						velocidade_saida.publish(vel)
-						rospy.sleep(0.8)
-						viu_dog = False
-						print("direita pra ir ao dog")
+			#Quad1,Quad2,Quad3,Quad4=split[0],split[1],split[2],split[3]
 
-					else:
-						vel = Twist(Vector3(0.4,0,0), Vector3(0,0,0))
-						velocidade_saida.publish(vel)
-						rospy.sleep(0.8)
-						viu_dog = False
-						print("Segue Dog")
-						continue
+			if min(Scaner)<0.2:
 
-                # vel = Twist(Vector3(0.4,0,0), Vector3(0,0,0))
-                # velocidade_saida.publish(vel)
-                # rospy.sleep(0.8)
-                # viu_cat = False
-                # continue
-			else:
-				if (len(mediaA) != 0 and len(centroA) != 0):# and len(mediaA) !=0  and len(centroA) !=0):
-					
-				#if areaA != 0 or areaV != 0:
-					#print("passei")
-					#if areaV > areaA:
-						
-					# 	if mediaV[0] < metade + sigma:
-					# 		vel = Twist(Vector3(0.1,0,0), Vector3(0,0,-0.3))
-					# 		#vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
-					# 		velocidade_saida.publish(vel)
-					# 		rospy.sleep(1.0)
-					# 		print("direitaa vermelho")
-						
-					# 	elif mediaV[0] > metade + sigma:
-					# 		vel = Twist(Vector3(0.1,0,0), Vector3(0,0,0.3))
-					# 		#vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
-					# 		velocidade_saida.publish(vel)
-					# 		rospy.sleep(1.0)
-					# 		print("esquerdaa vermelho")
-						
-					# 	else:
-					# 		vel = Twist(Vector3(0.1,0,0), Vector3(0,0,0))
-					# 		#vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
-					# 		velocidade_saida.publish(vel)
-					# 		rospy.sleep(1.0)
-					# 		print("em frente pro vermelho!")
-					
-					# elif areaV < areaA:
-						
-					if mediaA[0] < metade + sigma:
-						vel = Twist(Vector3(-0.1,0,0), Vector3(0,0,-0.3))
-						#vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
-						velocidade_saida.publish(vel)
-						rospy.sleep(1.0)
-						print("direitaa foge do azul")
-					
-					elif mediaA[0] > metade + sigma:
-						vel = Twist(Vector3(-0.1,0,0), Vector3(0,0,0.3))
-						#vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
-						velocidade_saida.publish(vel)
-						rospy.sleep(1.0)
-						print("esquerdaa run do azul")
-					
-					else:
-						vel = Twist(Vector3(-0.1,0,0), Vector3(0,0,0))
-						#vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
-						velocidade_saida.publish(vel)
-						rospy.sleep(1.0)
-						print("corre do azul berg!")
-				else:
-					vel = Twist(Vector3(0,0,0), Vector3(0,0,0.5))
+				if min(Quad1)<0.2:
+					vel = Twist(Vector3(-v,0,0), Vector3(0,0,w2))
 					velocidade_saida.publish(vel)
 					rospy.sleep(1.0)
-					print("TO VENO NADA!")
-				#print("Média dos vermelhos: {0}, {1}".format(media[0], media[1]))
-				#print("Centro dos vermelhos: {0}, {1}".format(centro[0], centro[1]))
-				#vel = Twist(Vector3(0.2,0,0), Vector3(0,0,0))
-				#print("aqui")
+					
+				elif min(Quad2)<0.2:
+					vel = Twist(Vector3(v,0,0), Vector3(0,0,w2))
+					velocidade_saida.publish(vel)
+					rospy.sleep(2.0)
+					
+					
+				elif min(Quad3)<0.2:
+					vel = Twist(Vector3(v,0,0), Vector3(0,0,-w2))
+					velocidade_saida.publish(vel)
+					rospy.sleep(1.0)
+					
+					
+				elif min(Quad4)<0.2:
+					vel = Twist(Vector3(-v,0,0), Vector3(0,0,-w2))
+					velocidade_saida.publish(vel)
+					rospy.sleep(2.0)
+					
+					
+				# else:
+				# 	vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
+				# 	velocidade_saida.publish(vel)
+				# 	rospy.sleep(1.0)
+
+			# DETECTOR MOBILENET - Friendly
+			elif viu_dog:
+
+				if posicao != None:
+
+					#print(" a media da posicao do dog eh{0}".format(media_dog))
+
+					if media_dog<metade-sigma:
+						vel = Twist(Vector3(v,0,0), Vector3(0,0,-w2))
+						velocidade_saida.publish(vel)
+						rospy.sleep(0.8)
+						viu_dog=False
+						print("seguindo dog indo pra esquerda")
+						continue
+
+					elif media_dog>metade+sigma:
+						vel = Twist(Vector3(v,0,0), Vector3(0,0,w2))
+						velocidade_saida.publish(vel)
+						rospy.sleep(0.8)
+						viu_dog = False
+						print("seguindo dog indo pra direita")
+						continue
+
+					else:
+						vel = Twist(Vector3(v,0,0), Vector3(0,0,0))
+						velocidade_saida.publish(vel)
+						rospy.sleep(1)
+						viu_dog = False
+						print("seguindo dog")
+						continue
+
+			# DETECTOR DE COR - Friendly
+
+			elif areaA>5000:
+
+				if mediaA[0] < metade - sigma:
+					vel = Twist(Vector3(-v,0,0), Vector3(0,0,-w2))
+					velocidade_saida.publish(vel)
+					rospy.sleep(1.0)
+					print("fugindo do azul desviando para direita")
+					continue
+				
+				elif mediaA[0] > metade + sigma:
+					vel = Twist(Vector3(-v,0,0), Vector3(0,0,w2))
+					velocidade_saida.publish(vel)
+					rospy.sleep(1.0)
+					print("fugindo do azul desviando para esquerda")
+					continue
+				else:
+					vel = Twist(Vector3(-v,0,0), Vector3(0,0,0))
+					velocidade_saida.publish(vel)
+					rospy.sleep(1.0)
+					print("fugindo do azul")
+					continue
+			else:
+
+				vel = Twist(Vector3(0,0,0), Vector3(0,0,w2))
 				velocidade_saida.publish(vel)
 				rospy.sleep(0.1)
-
+				print("Procurando a acao")
+				continue
 	except rospy.ROSInterruptException:
 	    print("Ocorreu uma exceção com o rospy")
 
